84dbce51dc0268dc7d506f0856c0ef95b67bc027
seovulf <seovulf@gmail.com>
2023-05-04 19:02:48 +0300

    be red

---
+++ app/src/main/java/com/example/sinemo/navigation/AppNavigation.kt
@@ -0,0 +1,16 @@
+package com.example.sinemo.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+
+@Composable
+fun AppNavigation(){
+    
+    NavHost(
+        navController =,
+        startDestination =
+    ){
+
+    }
+    
+}
