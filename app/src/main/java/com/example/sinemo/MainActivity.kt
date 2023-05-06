91919b98b9291007cf26c9a023cb5791d61996bc
seovulf <seovulf@gmail.com>
2023-05-07 02:13:16 +0300

    fixed navigation

--- app/src/main/java/com/example/sinemo/MainActivity.kt
+++ app/src/main/java/com/example/sinemo/MainActivity.kt
@@ -7,30 +7,20 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.rememberNavController
 import com.example.sinemo.navigation.AppNavigation
 import com.example.sinemo.navigation.AppScreen
+import com.example.sinemo.screens.MainScreen
 import com.example.sinemo.ui.theme.SinemoTheme
 import org.telegram.passport.*
-import java.io.File
+
 class MainActivity : ComponentActivity() {
     private lateinit var audioRecordViewModel: AudioRecordViewModel
     @SuppressLint("CoroutineCreationDuringComposition", "NewApi")
@@ -64,48 +54,6 @@ class MainActivity : ComponentActivity() {
                     },
                     content = {
                         AppNavigation(navController)
-                        Column(
-                            modifier = Modifier
-                                .background((Color.DarkGray))
-                                .fillMaxWidth()
-                                .fillMaxHeight(),
-                            verticalArrangement = Arrangement.Center,
-                            horizontalAlignment = Alignment.CenterHorizontally
-                        )
-                        {
-                            Button(onClick = { stopRecording()
-                                try {
-                                    val file = File(output)
-                                    if(file.exists()) {
-                                        val uri = FileProvider.getUriForFile(this@MainActivity, BuildConfig.APPLICATION_ID + ".provider", file)
-                                        val intent = Intent(Intent.ACTION_SEND)
-                                        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                                        intent.type = "audio/ogg"
-                                        intent.putExtra(Intent.EXTRA_STREAM, uri)
-                                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                                        intent.setPackage("org.telegram.messenger")
-                                        startActivity(intent)
-                                    }
-                                } catch (e: Exception) {
-                                    e.printStackTrace()
-                                }
-                            },
-                                colors = ButtonDefaults
-                                    .buttonColors(backgroundColor = Color.Black)) {
-                                Text(text = "Share emotion ")
-                                Icon(
-                                    imageVector = Icons.Default.Send,
-                                    contentDescription = null
-                                )
-                            }
-                            Button(onClick = {
-                                telepass()
-                                TelegramPassport.request(this@MainActivity, req, tgPassportResult)
-                            }, colors = ButtonDefaults
-                                .buttonColors(backgroundColor = Color.Cyan)) {
-                                Text(text = "Log in telegram", color = Color.Black)
-                            }
-                        }
                     },
                     bottomBar = {
                         BottomNavigation {
--- app/src/main/java/com/example/sinemo/screens/MainScreen.kt
+++ app/src/main/java/com/example/sinemo/screens/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.example.sinemo.screens
 
+import android.content.Context
 import android.content.Intent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -13,14 +14,59 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat.startActivity
 import androidx.core.content.FileProvider
+import androidx.navigation.NavGraphBuilder
 import com.example.sinemo.*
-import org.telegram.passport.TelegramPassport
 import java.io.File
 import java.lang.Exception
-private lateinit var audioRecordViewModel: AudioRecordViewModel
+
+//private lateinit var audioRecordViewModel: AudioRecordViewModel
 @Composable
-fun MainScreen(){
+fun MainScreen() {
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier
+            .background((Color.DarkGray))
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
+        Button(onClick = { stopRecording()
+            try {
+                val file = File(output)
+                if(file.exists()) {
+                    val uri = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".provider", file)
+                    val intent = Intent(Intent.ACTION_SEND)
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                    intent.type = "audio/ogg"
+                    intent.putExtra(Intent.EXTRA_STREAM, uri)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    intent.setPackage("org.telegram.messenger")
+                    context.startActivity(intent)
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        },
+            colors = ButtonDefaults
+                .buttonColors(backgroundColor = Color.Black)) {
+            Text(text = "Share emotion ")
+            Icon(
+                imageVector = Icons.Default.Send,
+                contentDescription = null
+            )
+        }
+        /*Button(onClick = {
+            telepass()
+            TelegramPassport.request(this@MainActivity, req, tgPassportResult)
+        }, colors = ButtonDefaults
+            .buttonColors(backgroundColor = Color.Cyan)) {
+            Text(text = "Log in telegram", color = Color.Black)
+        }*/
+    }
 
 }
