407492a8c224d3838252f135c639d08cc1bdd5d1
seovulf <seovulf@gmail.com>
2023-05-08 17:16:58 +0300

    added date format

--- app/src/main/java/com/example/sinemo/AudioViewModel.kt
+++ app/src/main/java/com/example/sinemo/AudioViewModel.kt
@@ -1,17 +1,25 @@
 package com.example.sinemo
 
+import android.annotation.SuppressLint
+import android.icu.text.SimpleDateFormat
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import java.util.Date
 
 class AudioViewModel: ViewModel() {
+    private val currentTimeMillis = System.currentTimeMillis()
+    private val date = Date(currentTimeMillis)
+    @SuppressLint("SimpleDateFormat")
+    private val dateFormat = SimpleDateFormat("HH:mm:ss dd/MM/yyyy ")
+    private val formattedDate = dateFormat.format(date)
 
     var recordsState by mutableStateOf(
         listOf(
             DataRecord(
-                heading = numRec.toString(),
-                subtext = System.currentTimeMillis().toString(),
+                heading = "recording$numRec",
+                subtext = formattedDate.toString(),
                 audioPath = output
             )
         )
